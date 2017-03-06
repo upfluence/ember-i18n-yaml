c300eec1b6b84c2eb87b585ef3e81566fdab9d47
tgallice <tmgallice@gmail.com>
2017-03-06 16:00:51 +0100

    init

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* jshint node: true */
 'use strict';
 
+var merge = require('deepmerge');
+var I18nYamlCompiler = require('./lib/i18n-yaml-compiler');
+
 module.exports = {
-  name: 'ember-i18n-yaml'
+  name: 'Ember i18n Yaml',
+  project: this.project,
+
+  i18nYamlConfig: function () {
+    var app = this.app;
+
+    if (app && !app.options && app.app) {
+      app = app.app;
+    }
+
+    return merge({
+        sourceDir: './config/locales',
+        distDir: './app/locales',
+        defaultLocal: 'en'
+      }, (app && app.options.i18nYaml) || {});
+  },
+
+  treeForApp: function() {
+    const compiler = new I18nYamlCompiler(this.i18nYamlConfig(), this.ui);
+    compiler.compile();
+  }
 };
